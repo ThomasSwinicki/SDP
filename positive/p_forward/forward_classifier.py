3ee0029761b987b1504f798b839256d829689e40
ThomasSwinicki <thomas.swinicki@uconn.edu>
2018-03-19 23:02:55 -0400

    Classifier script test, renaming script, and bash scripts for vector creation and classifier training

--- positive/p_forward/forward_classifier.py
+++ positive/p_forward/forward_classifier.py
@@ -9,10 +9,11 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cars = car_cascade.detectMultiScale(gray, 1.3, 5)
 
 for (x,y,w,h) in cars:
-	cv2.rectangle(img[x,y], (x+w, y+h), (255,0), 2)
-	roi_gray = gray[y:y+h, x:x+w]
-	roi_color = img[y:y+h, x:x+w]
+        cv2.rectangle(img[x,y], (x+w, y+h), (255,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
 
 cv2.imshow('img',img)
 cvw.waitKey(0)
 cv2.destroyAllWindows()
+
