dfd453d760e0feda886b21f5aca0eb04eca3dd0e
ThomasSwinicki <thomas.swinicki@uconn.edu>
2018-04-05 20:47:43 -0400

    more color detection

--- positive/alternativementhod/calibrateHSV.py
+++ positive/alternativementhod/calibrateHSV.py
@@ -3,77 +3,84 @@ import numpy as np
 import math
 import argparse
 
-img = cv2.imread("IMG_7596_small.jpg")
-img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-
-#image ROI are in [rows,columns] format => [y,x]
-red = img[250:300,300:400]
-yellow = img[350:400,300:400]
-green = img[440:490,300:400]
-blue = img[520:570,300:400]
-white = img[620:670,300:400]
-colors = [red, yellow, green, blue, white]
-
-for c in colors:
-	cv2.imshow('ROI', c)
-	cv2.waitKey(0)
-
-height, width= red.shape[:2]
-print(width)
-print(height) 
-vals = [[] for i in range(5)]
-#make i a tenth of the larger edge of the ROI
-for c in range(len(colors)):
-	for j in range(1,math.floor(width/10)):
-		for i in range(1, math.floor(height/10)):
-			#to access HSV of an image use image[x][y][z], where x,y,z are the HSV values resepctively
-			vals[c].append((colors[c][i*10][j*10][0], colors[c][i*10][j*10][1], colors[c][i*10][j*10][2]))
-
-#print(vals[0])
-#print('......\n')
-#print(vals[1])
-#print('......\n')
-#print(vals[2])
-#print('......\n')
-#print(vals[3])
-#print('......\n')
-#print(vals[4])
-#print('......\n')
-
-#get min and max of each
-colormins = [[360,360,360],[360,360,360],[360,360,360],[360,360,360],[360,360,360]]
-colormaxs = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-
-#for HSV in vals:
-#	j = 0
-#	for c in range(len(HSV)):
-#		for i in range(0,3):
-#			if(HSV[c][i] < colormins[j][i]):
-#				colormins[j][i] = HSV[c][i]
-#			if(HSV[c][i] > colormaxs[j][i]):
-#				colormaxs[j][i] = HSV[c][i]
-#	j+=1
-
-#vals = [ [3-tuples of HSV vals of red], [3-tuples of HSV vals of yellow], [...green], [...blue], [...white] ]
-for HSVlist in vals:
-	#using the 3-tuples of a color, HSV[0] is first tuple, HSV[0][0] is its hue, HSV[0][1] is its saturation, HSV[0][2] is its value
-	colorindex = vals.index(HSVlist)
-	for HSVvals in HSVlist:
-		#now we are using each tuple in each list of tuples for each color
-		for i in range(3):
-			if(HSVvals[i] > colormaxs[colorindex][i]):
-				colormaxs[colorindex][i] = HSVvals[i]
-			if(HSVvals[i] < colormins[colorindex][i]):
-				colormins[colorindex][i] = HSVvals[i]
-
-print(colormins)
-print(colormaxs)
-lowerhue = [colormins[0][0], colormins[1][0], colormins[2][0], colormins[3][0], colormins[4][0]]
-upperhue = [colormaxs[0][0], colormaxs[1][0], colormaxs[2][0], colormaxs[3][0], colormaxs[4][0]]
-print(lowerhue)
-print(upperhue)
+class Calibrator:
 	
+	#lowerhue = []
+	#upperhue = []
+	bounds = []
+
+	def calibrate(self):
+		img = cv2.imread("IMG_7596_small.jpg")
+		img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+
+		#image ROI are in [rows,columns] format => [y,x]
+		red = img[250:300,300:400]
+		yellow = img[350:400,300:400]
+		green = img[440:490,300:400]
+		blue = img[520:570,300:400]
+		white = img[620:670,300:400]
+		#red = img[290:310,300:400]
+		#yellow = img[380:400,300:400]
+		#green = img[470:490,300:400]
+		#blue = img[560:580,300:400]
+		#white = img[650:670,300:400]	
+		colors = [red, yellow, green, blue, white]
+
+		for c in colors:
+			cv2.imshow('ROI', c)
+			cv2.waitKey(0)
+
+		height, width= red.shape[:2]
+		vals = [[] for i in range(5)]
+		#make i a tenth of the larger edge of the ROI
+		for c in range(len(colors)):
+			for j in range(1,math.floor(width/12)):
+				for i in range(1, math.floor(height/12)):
+					#to access HSV of an image use image[x][y][z], where x,y,z are the HSV values resepctively
+					vals[c].append((colors[c][i*12][j*12][0], colors[c][i*12][j*12][1], colors[c][i*12][j*12][2]))
 
 
-def hueRange:
-	return(lowerhue,upperhue)
+		#get min and max of each
+		colormins = [[360,360,360],[360,360,360],[360,360,360],[360,360,360],[360,360,360]]
+		colormaxs = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+		#vals = [ [3-tuples of HSV vals of red], [3-tuples of HSV vals of yellow], [...green], [...blue], [...white] ]
+		for HSVlist in vals:
+			#using the 3-tuples of a color, HSV[0] is first tuple, HSV[0][0] is its hue, HSV[0][1] is its saturation, HSV[0][2] is its value
+			colorindex = vals.index(HSVlist)
+			for HSVvals in HSVlist:
+				#now we are using each tuple in each list of tuples for each color
+				#for i in range(3):
+				#	if(HSVvals[i] > colormaxs[colorindex][i]):
+				#		colormaxs[colorindex][i] = HSVvals[i]
+				#	if(HSVvals[i] < colormins[colorindex][i]):
+				#		colormins[colorindex][i] = HSVvals[i]
+				for i in range(1):
+					if(HSVvals[i] > colormaxs[colorindex][i]):
+						colormaxs[colorindex][i] = HSVvals[i]
+					if(HSVvals[i] < colormins[colorindex][i]):
+						colormins[colorindex][i] = HSVvals[i]
+				for i in range(1,3):
+					colormaxs[colorindex][i] = 255
+					colormins[colorindex][i] = 0
+
+		#self.lowerhue = [colormins[0][0], colormins[1][0], colormins[2][0], colormins[3][0], colormins[4][0]]
+		#self.upperhue = [colormaxs[0][0], colormaxs[1][0], colormaxs[2][0], colormaxs[3][0], colormaxs[4][0]]
+		huemins = []
+		huemaxs = []
+		#for h in colormins:
+		#	huemins.append(h[0]);
+		#for h in colormaxs:
+		#	huemaxs.append(h[0]);
+		#print(huemins)
+		#print(huemaxs)
+		for i in range(5):
+			self.bounds.append((colormins[i],colormaxs[i]))
+			#self.bounds.append((huemins[i],huemaxs[i]))	
+
+	def __init__(self):
+		self.calibrate()
+
+
+	def hueRange(self):
+		return(self.bounds)
--- positive/alternativementhod/colorthenshape.py
+++ positive/alternativementhod/colorthenshape.py
@@ -1,4 +1,5 @@
 from shapedetector import ShapeDetector
+from calibrateHSV import Calibrator
 import numpy as np
 import cv2
 import argparse
@@ -9,18 +10,31 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 
+#for HSV
 imgin = cv2.imread(args["image"])
-
 img = cv2.cvtColor(imgin, cv2.COLOR_RGB2HSV);
 
-#list of boundaries for the colors in the following order: Blue, Green, Yellow, Red, White
-boundaries = [
-        ([100, 41, 21], [192, 102, 70]),
-        ([41, 68, 24], [112,166,106]),
-        ([70, 183, 120], [115, 253, 255]),
-        ([29,39,140], [100,100,255]),
-        ([160, 180, 185], [255, 255, 255])
-]
+#for RGB
+#img = cv2.imread(args["image"])
+
+calib = Calibrator()
+boundaries = calib.hueRange()
+print(boundaries)
+#boundaries = [(220,250),(85,140),(40,70),(0,20),(65,115)]
+#boundaries = [([115,100,195] , [120,190,230]),([89,0,0] , [94,255,255]),([42,0,0] , [47,255,255]),([5,0,0] ,[15,255,255]),([95,0,0],[110,255,255])]
+#boundaries = [([110,0,0] , [120,255,255]),([89,0,0] , [94,255,255]),([42,0,0] , [47,255,255]),([5,0,0] ,[15,255,255]),([95,0,0],[110,255,255])]
+#boundaries = [([110,0,50] , [120,255,255]),([89,0,50] , [94,255,255]),([42,0,50] , [47,255,255]),([5,0,50] ,[15,255,255]),([95,0,50],[110,255,255])]
+
+#list of RGB (BGR in numpy)boundaries for the colors in the following order: Blue, Green, Yellow, Red, White
+#boundaries = [
+#        ([100, 41, 21], [192, 102, 70]),
+#        ([41, 68, 24], [112,166,106]),
+#        ([70, 183, 120], [115, 253, 255]),
+#        ([29,39,140], [100,100,255]),
+#        ([160, 180, 185], [255, 255, 255])
+#]
+
+
 
 #array for the number of times an instruction has been output to the text file, to keep track of which instructions have already been accounted for 
 instructs = [] 
@@ -38,7 +52,12 @@ for (lower, upper) in boundaries:
 	resized = imutils.resize(output, width=300)
 	ratio = output.shape[0] / float(resized.shape[0])
 
-	gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+	#for HSV
+	temp = cv2.cvtColor(resized, cv2.COLOR_HSV2RGB)
+	gray = cv2.cvtColor(temp, cv2.COLOR_RGB2GRAY)
+	
+	#for RGB
+	#gray = cv2.cvtColor(resized, cv2.COLOR_RGB2GRAY)
 	blurred = cv2.GaussianBlur(gray, (5,5), 0)
 	thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 	
