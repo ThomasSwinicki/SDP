893b972340594e499a353edd5ecdb542a80e7dfe
ThomasSwinicki <thomas.swinicki@uconn.edu>
2018-04-16 18:09:12 -0400

    Dependencies bash

---
+++ positive/alternativementhod/dependencies.bash
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get --assume-yes upgrade
+sudo apt-get --assume-yes install tesseract-ocr
+sudo apt-get --assume-yes install imagemagick 
+sudo apt --assume-yes install python3-pip
+sudo pip3 install --upgrade pip
+sudo pip3 install flask
+sudo pip3 install imutils
+sudo pip3 install argparse
+sudo pip3 install opencv-python
+sudo apt-get upgrade
